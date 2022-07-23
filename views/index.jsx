6676440773a11228bc41ebcfb95529e2e7dff453
Wade Graves <wadegraves940@gmail.com>
2022-07-23 12:03:19 -0500

    fixed

--- views/index.jsx
+++ views/index.jsx
@@ -8,11 +8,18 @@ function Index ({breads})  {
         {/* This is a JSX comment. */}
         {/* <p>I have {breads[0].name} bread!</p> */}
         <ul>
-            {
-                breads.map((bread, index) => {
-                    return (<li key={index}><a href={'/breads/${index}'}>{bread.name}</a></li>)
-                })
-            }
+        {
+  breads.map((bread, index)=> {
+    return (
+      <li key={index}>
+        <a href={`/breads/${index}`}>
+          {bread.name}
+        </a>
+      </li>
+    )
+  })
+}
+
         </ul> 
       </Default>
       
