6a10e4ea43b8f6d90f7ebcde1504572aeb8f2cb2
Wade Graves <wadegraves940@gmail.com>
2022-07-27 20:26:49 -0500

    edit and updateroutes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -29,6 +29,13 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -41,6 +48,21 @@ breads.get('/:arrayIndex', (req, res) => {
   res.send('404')
   }
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
+
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
